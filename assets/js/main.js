f09c1b44c8bd8871bd23c1d348cf4d368dc74aa7
Finnschy <skylerfearless@yahoo.de>
2021-10-19 20:52:11 +0200

    almost done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+// tutorial von Kevin Powell https://www.youtube.com/watch?v=gBzsE0oieio&ab_channel=KevinPowell
+
 const track = document.querySelector('.carousel__track');
 const slides = Array.from(track.children);
 const nextButton = document.querySelector('.carousel__button--right');
@@ -13,6 +15,7 @@ const setSlidePosition = (slide, index) => {
 }
 slides.forEach(setSlidePosition);
 
+// diese hilfsfunktion fasst alles zusammen, was geschehen soll wenn auf einen der button geklickt wird
 const moveToSlide = (track, currentSlide, targetSlide) => {
     track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
     currentSlide.classList.remove('current-slide');
@@ -35,4 +38,17 @@ nextButton.addEventListener('click', e => {
     moveToSlide(track, currentSlide, nextSlide);
 })
 
+// wenn die nav indicators geklickt werden, soll zu dem slide gewechselt werden
 
+dotsNav.addEventListener('click', e => {
+    // welcher indicator wurde geklickt?
+    const targetDot = e.target.closest('button');
+    
+    if (!targetDot) return;
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide')
+    const targetIndex = dots.findIndex(dot => dot === targetDot)
+    const targetSlide  = slides[targetIndex];
+    
+    moveToSlide(track, currentSlide, targetSlide)
+})
